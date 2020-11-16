43057167ccc8cc97fdd39cb44789b05361d71bcc
bai <baichaofeng0520@163.com>
2020-11-16 11:37:23 +0800

    图片压缩

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -64,6 +64,17 @@ module.exports = {
           limit: 10000,
           name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
         }
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+       // loader: 'url-loader',
+        // options: {
+            // limit: 100000,
+            // name: utils.assetsPath('img/[name].[hash:7].[ext]')
+        // }
+        loader: ['url-loader?limit=10000&name=' + utils.assetsPath('img/[name].[hash:7].[ext]'),
+            'image-webpack-loader'
+        ]
       }
     ]
   },
